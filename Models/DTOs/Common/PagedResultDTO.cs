b60d18fe5f9f338087e8cdab94ccdd3f1b3dda52
sawn05 <sanglaida@gmail.com>
2026-05-21 00:02:47 +0700

    Initial Order DTO

--- Models/DTOs/Common/PagedResultDTO.cs
+++ Models/DTOs/Common/PagedResultDTO.cs
@@ -4,7 +4,7 @@
     {
         public List<T> Items { get; set; } = new();
         public int TotalItems { get; set; }
-        public int TotalPages { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalItems / PageSize);
         public int CurrentPage { get; set; }
         public int PageSize { get; set; }
     }
---
+++ Models/DTOs/Order/OrderItemDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Bookstore.Models.DTOs.Order
+{
+    public class OrderItemDTO
+    {
+        [Required]
+        public Guid BookId { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Số lượng phải ít nhất là 1")]
+        public int Quantity { get; set; }
+    }
+}
